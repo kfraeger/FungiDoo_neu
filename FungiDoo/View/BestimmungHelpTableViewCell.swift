38752dff11dfe536f9dea52c1f026de82c53017a
Katja Fraeger <katjafraeger@katjas-mbp.fritz.box>
2018-10-26 22:24:37 +0200

    Bestimmungshilfe

---
+++ FungiDoo/View/BestimmungHelpTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BestimmungHelpTableViewCell.swift
+//  FungiDoo
+//
+//  Created by Katja Fraeger on 26.10.18.
+//  Copyright © 2018 Katja Fraeger. All rights reserved.
+//
+
+import UIKit
+
+class BestimmungHelpTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageViewCell: UIImageView!
+    @IBOutlet weak var labelCell: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
